b63b867e79dbeaf9ce5ac0311a7fed817835dd35
Diego Veras <verasdiegoca78@gmail.com>
2025-01-08 20:18:03 -0300

    Add allowed origin for CORS configuration in SecurityConfig

--- server/src/main/java/com/vocaltech/api/config/security/SecurityConfig.java
+++ server/src/main/java/com/vocaltech/api/config/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8090"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8090", "https://vocaltech.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
