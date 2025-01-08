cf3006e96ba61d0bf540dc08386e4437cad7df8a
Diego Veras <verasdiegoca78@gmail.com>
2025-01-08 14:52:15 -0300

    Add exception classes and repository interfaces for role and user management

---
+++ server/src/main/java/com/vocaltech/api/repository/IUserRepository.java
@@ -0,0 +1,4 @@
+package com.vocaltech.api.repository;
+
+public interface IUserRepository {
+}
---
+++ server/src/main/java/com/vocaltech/api/service/implementation/AuthServiceImpl.java
@@ -0,0 +1,4 @@
+package com.vocaltech.api.service.implementation;
+
+public class AuthServiceImpl {
+}
