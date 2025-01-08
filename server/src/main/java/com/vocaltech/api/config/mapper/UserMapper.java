2132bd299981bc792b98397a9c8c51ec3b198203
Diego Veras <verasdiegoca78@gmail.com>
2025-01-08 19:54:18 -0300

    Remove unused method overloads in UserMapper interface

--- server/src/main/java/com/vocaltech/api/config/mapper/UserMapper.java
+++ server/src/main/java/com/vocaltech/api/config/mapper/UserMapper.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
     UserResponseDto toUserResponseDTO(User user);
-
     List<UserResponseDto> toUserResponseDTO(List<User> user);
 }
