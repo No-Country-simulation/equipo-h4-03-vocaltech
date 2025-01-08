f2563c310042060f124504efde5a8cabe9b4087f
Diego Veras <verasdiegoca78@gmail.com>
2025-01-08 15:19:12 -0300

    Add AuthenticationConfig and CustomUserDetailsService for user authentication

--- server/src/main/java/com/vocaltech/api/config/security/CustomUserDetailsService.java
+++ server/src/main/java/com/vocaltech/api/config/security/CustomUserDetailsService.java
@@ -1,4 +1,21 @@
 package com.vocaltech.api.config.security;
 
-public class CustomUserDetailsService {
+import com.vocaltech.api.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final IUserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findUserByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(String.format("User not found with email: %s.", email)));
+    }
 }
