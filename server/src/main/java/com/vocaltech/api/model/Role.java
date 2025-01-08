9bd1bd5a0fe71fcf39a862937d5e25614f69c116
Diego Veras <verasdiegoca78@gmail.com>
2025-01-08 13:39:29 -0300

    Add Role and User entity classes with JPA annotations

--- server/src/main/java/com/vocaltech/api/model/Role.java
+++ server/src/main/java/com/vocaltech/api/model/Role.java
@@ -1,4 +1,21 @@
 package com.vocaltech.api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "role_id")
+    private UUID roleId;
+
+    @Column(unique = true, nullable = false)
+    private String name;
 }
